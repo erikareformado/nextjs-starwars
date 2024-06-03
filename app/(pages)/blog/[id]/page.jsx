72a180a340bad0bf2943df4cd7539450db20dff2
Erika <79175741+erikareformado@users.noreply.github.com>
2024-06-03 18:59:20 +0800

    feat: add initial blog show

---
+++ app/(pages)/blog/[id]/page.jsx
@@ -0,0 +1,32 @@
+    export async function getStaticPaths() {
+
+    // Call an external API endpoint to get posts
+    const posts =  await fetch('https://jsonplaceholder.typicode.com/posts').then((res) => res.json())
+    // const posts = await res.json()
+   
+    // Get the paths we want to prerender based on posts
+    // In production environments, prerender all pages
+    // (slower builds, but faster initial page load)
+    const paths = posts.map((post) => ({
+      params: { id: post.id.toString() },
+    }))
+   
+    // { fallback: false } means other routes should 404
+    return { paths, fallback: false }
+  }
+
+  export async function generateStaticParams() {
+    const posts = await fetch('https://jsonplaceholder.typicode.com/posts').then((res) => res.json())
+    return posts.map((post) => ({
+      slug: post.id,
+    }))
+  }
+
+  const BlogShow = () => {
+    return (
+        <>
+        </>
+    )
+  }
+
+  export default BlogShow
